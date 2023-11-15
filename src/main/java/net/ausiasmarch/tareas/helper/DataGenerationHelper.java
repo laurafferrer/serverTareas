2491a2a5c322ae58ac0d3e9d51ed5f9ab1013b2d
Laura <lferrere.ausiasmarch@gmail.com>
2023-11-15 12:39:42 +0100

    modificar helper!!

--- src/main/java/net/ausiasmarch/tareas/helper/DataGenerationHelper.java
+++ src/main/java/net/ausiasmarch/tareas/helper/DataGenerationHelper.java
@@ -4,15 +4,9 @@ import java.util.Random;
 
 public class DataGenerationHelper {
 
-    private static final String[] aNames = { "Mónica", "José Antonio", "Laura", "Lucas", "Eva", "Eloy", "Jesús", "Alan",
-            "Pablo", "Paula", "Raquel", "Nieves", "Elena", "Sergio", "Jaime", "Fernando", "Rafael" };
+    private static final String[] aNames = { "Alberto", "Marta", "Carlos" };
 
-    private static final String[] aSurnames = {
-            "Alcañiz", "Puig", "Ayala", "Farell", "Ferrer", "Esteve", "González", "Rozalén", "Lara", "Velarte",
-            "Latorre", "Briones", "Maldonado", "Suárez", "McLure", "Alarcón", "Molero", "Marín", "Muñoz", "García",
-            "Navarro", "López", "Navas", "Aguilar", "Ortega", "Fabra", "Romero", "Díaz", "Cano", "Roselló", "Gómez",
-            "Serrano", "Quilez", "Aznar", "Aparici"
-    };
+    private static final String[] aSurnames = { "García", "López", "González" };
 
     public static String getRadomName() {
         return aNames[(int) (Math.random() * aNames.length)];
