5651f59527df286a017598480067bf161dcd5e5d
Laura Ferrer <lferrere.ausiasmarch@gmail.com>
2023-11-21 11:39:30 +0100

    base de datos bien

--- docs/tareas.sql
+++ docs/tareas.sql
@@ -1,101 +1,62 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: database:3306
--- Tiempo de generación: 17-11-2023 a las 17:44:03
--- Versión del servidor: 8.1.0
--- Versión de PHP: 8.2.11
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+-- Crear base de datos: `tareas`
+CREATE DATABASE tareas;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+USE tareas;
 
---
--- Base de datos: `tareas`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `proyecto`
---
-
-CREATE TABLE `proyecto` (
-  `id` bigint NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `fecha_inicio` datetime NOT NULL,
-  `fecha_fin` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tarea`
---
-
-CREATE TABLE `tarea` (
-  `id` bigint NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `proyecto_id` int NOT NULL,
-  `usuario_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `usuario`
---
-
 CREATE TABLE `usuario` (
-  `id` bigint NOT NULL,
-  `codigo` bigint NOT NULL,
+  `id` bigint AUTO_INCREMENT PRIMARY KEY,
   `nombre` varchar(255) NOT NULL,
   `apellidos` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `puesto` tinyint(1) NOT NULL,
   `password` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Índices para tablas volcadas
---
+-- Estructura de tabla para la tabla `proyecto`
+CREATE TABLE `proyecto` (
+  `id` bigint AUTO_INCREMENT PRIMARY KEY,
+  `nombre` varchar(255) NOT NULL,
+  `fecha_inicio` datetime NOT NULL,
+  `fecha_fin` datetime DEFAULT NULL,
+  `usuario_id` BIGINT,
+  FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Indices de la tabla `proyecto`
---
-ALTER TABLE `proyecto`
-  ADD PRIMARY KEY (`id`);
+-- Estructura de tabla para la tabla `tarea`
+CREATE TABLE `tarea` (
+  `id` bigint AUTO_INCREMENT PRIMARY KEY,
+  `nombre` varchar(255) NOT NULL,
+  `proyecto_id` BIGINT NOT NULL,
+  `usuario_id` BIGINT NOT NULL,
+  FOREIGN KEY (proyecto_id) REFERENCES proyecto(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Indices de la tabla `tarea`
---
-ALTER TABLE `tarea`
-  ADD PRIMARY KEY (`id`);
+-- Insertar datos en la tabla `usuario`
+INSERT INTO usuario (nombre, apellidos, username, puesto, password) 
+VALUES ('Juan', 'Perez', 'juanperez', 1, '123456');
 
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `codigo` (`codigo`);
+INSERT INTO usuario (nombre, apellidos, username, puesto, password) 
+VALUES ('Maria', 'Lopez', 'marialopez', 0, '123456');
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
+INSERT INTO usuario (nombre, apellidos, username, puesto, password)
+VALUES ('Pedro', 'Gomez', 'pedrogomez', 0, '123456');
 
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `codigo` bigint NOT NULL AUTO_INCREMENT;
-COMMIT;
+-- Insertar datos en la tabla 'proyecto'
+INSERT INTO proyecto (nombre, fecha_inicio, fecha_fin, usuario_id)
+VALUES ('Proyecto 1', '2023-11-20', '2023-11-24', 1);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO proyecto (nombre, fecha_inicio, fecha_fin, usuario_id)
+VALUES ('Proyecto 2', '2023-11-21', '2023-11-30', 2);
+
+-- Insertar datos en la tabla 'tarea'
+INSERT INTO tarea (nombre, proyecto_id, usuario_id)
+VALUES ('Tarea 1', 1, 1);
+
+INSERT INTO tarea (nombre, proyecto_id, usuario_id)
+VALUES ('Tarea 2', 1, 2);
